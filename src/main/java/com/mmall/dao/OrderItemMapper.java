38d0c22467ae198cfa12fc506a8caabbb05b0eca
hanbingMu <1595516750@qq.com>
2017-11-25 19:50:11 +0800

    finish code and test for order moudel

--- src/main/java/com/mmall/dao/OrderItemMapper.java
+++ src/main/java/com/mmall/dao/OrderItemMapper.java
@@ -19,4 +19,10 @@ public interface OrderItemMapper {
     int updateByPrimaryKey(OrderItem record);
 
     List<OrderItem> getByOrderNoUserId(@Param("orderNo")Long orderNo, @Param("userId")Integer userId);
+
+    void batchInsert(@Param("orderItemList") List<OrderItem> orderItemList);
+
+    List<OrderItem> getByOrderNo(@Param("orderNo")Long orderNo);
+
+
 }
